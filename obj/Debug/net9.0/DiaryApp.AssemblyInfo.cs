b7ef0f9bb55116240dc5ded920b0be1763ca822e
Kala Subramani <kalasubramani@gmail.com>
2024-11-30 16:28:24 -0500

    Add new record functionality - added new button and styles to MyDiary page

--- obj/Debug/net9.0/DiaryApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/DiaryApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiaryApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6804c47beaee483f7bdb67758b9c126d4d56d2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69255b8b6fba269937f47f850bccacd498867648")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiaryApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiaryApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
