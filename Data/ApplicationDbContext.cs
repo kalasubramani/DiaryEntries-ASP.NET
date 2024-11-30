f704239394b12bfdf23e01d61081a5cc4bde6f5c
Kala Subramani <kalasubramani@gmail.com>
2024-11-29 20:03:21 -0500

    ASP.NET Refresher course - ASP.NET Core 9

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using DiaryApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiaryApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options) 
+        {
+            
+        }
+        //Steps to add a table to DB
+        //1. Create a model class
+        //2. Add DB Set
+        //3. add-migration AddDiaryEntryTable -> an instruction on how to modify the DB
+        //4. update-database
+
+        //create a table in DB with name Diary entry and creates columns relative to the properties in model class
+        public DbSet<DiaryEntry> DiaryEntries { get; set; }
+    }
+}
---
+++ Models/DiaryEntry.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DiaryApp.Models
+{
+    public class DiaryEntry
+    {
+        //create property to match the DB column names
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }=string.Empty;//set default value for required props or set it as non-nullable with string?
+
+        [Required]
+        public string Content { get; set; }=string.Empty;
+
+        [Required]
+        public DateTime Created { get; set; }=DateTime.Now;
+    }
+}
