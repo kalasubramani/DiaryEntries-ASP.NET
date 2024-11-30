f6804c47beaee483f7bdb67758b9c126d4d56d2d
Kala Subramani <kalasubramani@gmail.com>
2024-11-30 16:01:30 -0500

    Added logic to read data from DB and display it in the web app

--- Controllers/DiaryEntriesController.cs
+++ Controllers/DiaryEntriesController.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DiaryApp.Data;
+using DiaryApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DiaryApp.Controllers
 {
     public class DiaryEntriesController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+
+        //dependeny injection of DB contxt
+        public DiaryEntriesController(ApplicationDbContext db)
+        {
+            _db= db;            
+        }
         public IActionResult Index()
         {
-            return View();
+            List<DiaryEntry> objDiaryEntryList = _db.DiaryEntries.ToList();
+            //pass data to view
+            return View(objDiaryEntryList);
         }
     }
 }
